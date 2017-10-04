4f4af548a5c1f69a5e231a537b3bc633c6706af5
Kacper <kacper.kogut16@gmail.com>
2017-10-04 22:22:12 +0200

    Project init

---
+++ app/src/main/java/com/kacper/and_krakgo/KrakGoApp.java
@@ -0,0 +1,13 @@
+package com.kacper.and_krakgo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KrakGoApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_krak_go_app);
+    }
+}
